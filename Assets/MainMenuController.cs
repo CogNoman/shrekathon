b2fe249a7c371f9fe82911292c883408ce9846a3
Rachel <rachelh0205@gmail.com>
2026-03-21 20:25:12 -0400

    Adding a majority of the scenes and some of the transitions

--- Assets/MainMenuController.cs
+++ Assets/MainMenuController.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuController : MonoBehaviour
+
+public class TransitionControl : MonoBehaviour
 {
-    
+    public void StartGame() 
+    {
+        // Insert start game logic
+        SceneManager.LoadScene("Prelude");
+    }
+
+    public void ExitGame()
+    {
+        // Exit game logic
+    }
+
+    public void Tutorial()
+    {
+    // Open tutorial
+    }
+
+    public void Credits() 
+    {
+    // Open credits
+    }
+
+    public void LoadNextScene() 
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
 }
---
+++ Assets/Scripts/TransitionController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TransitionController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //Debug.Log("Starting the game...");
+        SceneManager.LoadScene("Prelude1");
+    }
+
+    public void ExitGame()
+    {
+        //Debug.Log("Closing game...");
+        Application.Quit();
+
+    }
+
+    public void Tutorial()
+    {
+        // Open tutorial
+    }
+
+    public void Credits()
+    {
+        // Open credits
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("AdditiveMAIN");
+    }
+
+    public void GameOverExit() 
+    {
+        // Takes user back to main menu where
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
+
