fd51fdcf2444eaf9250dc05e07225179b604fe00
CogNoman <cognoman@hotmail.com>
2026-03-22 13:15:57 -0400

    Add makeupSpawner

---
+++ Assets/Scripts/makeupScene/ItemSpawner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public Vector2 minSpawnBounds;
+    public Vector2 maxSpawnBounds;
+
+    void Start()
+    {
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.OnItemAdded += SpawnItem;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (InventoryManager.Instance != null)
+        {
+            InventoryManager.Instance.OnItemAdded -= SpawnItem;
+        }
+    }
+
+    void SpawnItem(InventoryItemData item)
+    {
+        Vector2 randomPos = new Vector2(
+            Random.Range(minSpawnBounds.x, maxSpawnBounds.x),
+            Random.Range(minSpawnBounds.y, maxSpawnBounds.y)
+        );
+
+        GameObject obj = new GameObject(item.itemName);
+        var sr = obj.AddComponent<SpriteRenderer>();
+        sr.sprite = item.bigSprite;
+
+        obj.transform.position = randomPos;
+    }
+}
