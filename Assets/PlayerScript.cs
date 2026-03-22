cf21071f637085398c5e512dcd51e840f8e20b31
Tommy <tomivo1212@hotmail.com>
2026-03-21 22:21:38 -0400

    started working on spin attack

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float baseMovementSpeed, movementSpeedModifier, movementSpeed, armor, rageMeter;
+    public float baseMovementSpeed, movementSpeedModifier, movementSpeed, armor, rageMeter, attackRadius = 2f, spinCooldown = 0f;
     public bool armored;
     public bool alive;
     public bool enraged;
@@ -22,6 +24,33 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         movementSpeed = baseMovementSpeed * movementSpeedModifier;
+
+        if (spinCooldown > 0f)
+        {
+            spinCooldown -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && spinCooldown <= 0)
+        {
+            SpinAttack();
+            spinCooldown = 4f;
+        }
+
+        void SpinAttack()
+        {
+            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, attackRadius);
+
+            foreach (Collider2D hits in hits)
+            {
+                if (hits.CompareTag("Enemy"))
+                {
+                    // placeholder effect, should change texture. and make them immobile
+                    // i stopped here, resume! also removes their hitbox so things can pass through.
+                    // make sure that when they die, they have a chance to drop equipment!
+                    hits.alive = false;
+                }
+            }
+        }
 
         if (armor <= 0)
         {
